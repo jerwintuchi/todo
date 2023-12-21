87e8628c86155ce7058fb63a0ccee5397e848ddf
unknown <jerwin.tuchi3@gmail.com>
2023-12-21 22:40:52 +0800

    updated the status indicator

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import Header from "./components/Header";
-import Form from "./components/Form";
-import {useState} from "react";
-import TodoList from "./components/TodoList"
-import style from "./styles.module.css"
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+import style from './styles.module.css';
 
 function App() {
-  const [todo,setTodo] = useState('');
-  const [todoList,setTodoList] = useState([]);
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
   const doneCount = todoList.filter(item => item.isDone).length;
+  const totalTasks = todoList.length;
+
+  let taskMessage,doneStatus;
+  if (totalTasks === 0) {
+    taskMessage = 'Add some tasks to do!';
+  } else if (doneCount === totalTasks) {
+    taskMessage = 'All tasks are done! :)';
+  } else {
+    taskMessage = `Things to do: ${totalTasks - doneCount}`;
+    doneStatus = `Done: ${doneCount}`;
+  }
 
   return (
     <div className="App">
-      <Header></Header>
-      <Form 
-        todo={todo} 
+      <Header />
+      <Form
+        todo={todo}
         setTodo={setTodo}
         todoList={todoList}
         setTodoList={setTodoList}
-      ></Form>
+      />
       <div className={style.todoCounters}>
-        <div className={style.counter}>Things to do: {todoList.length - doneCount}</div>
-        <div className={style.counter}>Done: {doneCount}</div>
+        <div className={style.counter}>{taskMessage}</div>
+        <div className={style.counter}>{doneStatus}</div>
       </div>
-      <TodoList 
-      setTodoList={setTodoList}
-      todoList={todoList} />
+      <TodoList setTodoList={setTodoList} todoList={todoList} />
     </div>
   );
 }
